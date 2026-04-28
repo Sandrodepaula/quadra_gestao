0c092ff26904e1c8e1cc2e705c927b8aa38e46a6
Sandro de Paula <100056992+Sandrodepaula@users.noreply.github.com>
2026-04-27 22:27:09 -0300

    Estruturação de pastas

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// Aqui é o ponto de entrada da aplicação. Ele é responsável por configurar o servidor, 
+// conectar ao banco de dados e iniciar a aplicação. Ele importa o app, que é onde as rotas 
+// e middlewares estão configurados, e o pool de conexões do banco de dados para garantir que a 
+// aplicação só inicie após uma conexão bem-sucedida com o banco.
+
+const app = require("./src/app");
+const { pool } = require("./src/config/database");
+
+const port = 3000;
+
+pool.connect()
+    .then(() => {
+        console.log("Banco conectado");
+        app.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Erro ao conectar ao banco:", err);
+        process.exit(1);
+    });
