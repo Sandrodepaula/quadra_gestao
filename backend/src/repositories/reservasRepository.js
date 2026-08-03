466d796af5f977857bac2e40e0c7bc6be742224e
Sandro de Paula <100056992+Sandrodepaula@users.noreply.github.com>
2026-08-03 20:10:57 -0300

    Ajusta métodos de listar, criar e atualizar no repositório de reservas para incluir o id e remover a verificação de conflitos

--- backend/src/repositories/reservasRepository.js
+++ backend/src/repositories/reservasRepository.js
@@ -3,15 +3,12 @@ const { pool } = require("../config/database");
 
 class reservasRepository {
 
-    async listar (){
-        const { rows} = await pool.query(
-            // Verificar se há reservas conflitantes para a mesma quadra, data e horário
-            `SELECT * FROM reservas 
-            WHERE quadraId = ? AND data = ? AND hora_inicio < :hora_fim AND hora_fim > :hora_inicio`,
-            [quadraId, data, hora_inicio, hora_fim]
+    async listar() {
+        const { rows } = await pool.query(
+            `SELECT * FROM reservas ORDER BY data ASC, hora_inicio ASC`
         );
         return rows;
-    };
+    }
 
     async buscar (id) {
         const { rows} = await pool.query("SELECT * FROM reservas WHERE id = $1", [id]);
@@ -20,19 +17,19 @@ class reservasRepository {
 
 
     async criar (dados) {
-        const { clienteId, quadraId, data, hora_inicio, hora_fim } = dados;
+        const { id, clienteId, data, hora_inicio, hora_fim } = dados;
         const { rows } = await pool.query(
-            "INSERT INTO reservas (cliente_id, quadra_id, data, hora_inicio, hora_fim) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-            [clienteId, quadraId, data, hora_inicio, hora_fim]
+            "INSERT INTO reservas (id, cliente_id, data, hora_inicio, hora_fim) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [id, clienteId, data, hora_inicio, hora_fim]
         );
         return rows[0];
     }
 
-    async atualizar (id, dados) {
-        const { clienteId, quadraId, data, hora_inicio, hora_fim } = dados;
+    async atualizar ( dados) {
+        const { id, clienteId, data, hora_inicio, hora_fim } = dados;
         const { rows } = await pool.query(
-            "UPDATE reservas SET cliente_id = $1, quadra_id = $2, data = $3, hora_inicio = $4, hora_fim = $5 WHERE id = $6 RETURNING *",
-            [clienteId, quadraId, data, hora_inicio, hora_fim, id]
+            "UPDATE reservas SET id = $1, cliente_id = $2, data = $3, hora_inicio = $4, hora_fim = $5 WHERE id = $6 RETURNING *",
+            [id, clienteId, data, hora_inicio, hora_fim]
         );
         return rows[0];
     }
